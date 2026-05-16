9680494772926e71a07c800e25e1725edf34751b
lokeshyadavalli001-star <lokeshyadavalli001@gmail.com>
2026-05-16 07:54:20 +0530

    Create preprocessing.py

---
+++ src/preprocessing.py
@@ -0,0 +1,50 @@
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import (
+StandardScaler,
+OneHotEncode
+)
+def build_preprocessor(numeric_features,categorical_features):
+  numeric_pipeline = Pipeline(
+        steps=[
+            (
+                "imputer",
+                SimpleImputer(strategy="median")
+            ),
+            (
+                "scaler",
+                StandardScaler()
+            )
+        ]
+    )
+  categorical_pipeline = Pipeline(
+        steps=[
+            (
+                "imputer",
+                SimpleImputer(strategy="most_frequent")
+            ),
+            (
+                "encoder",
+                OneHotEncoder(
+                    handle_unknown="ignore"
+                )
+            )
+        ]
+  )
+   preprocessor = ColumnTransformer(
+        transformers=[
+            (
+                "num",
+                numeric_pipeline,
+                numeric_features
+            ),
+            (
+                "cat",
+                categorical_pipeline,
+                categorical_features
+            )
+        ]
+    )
+
+    return preprocessor
